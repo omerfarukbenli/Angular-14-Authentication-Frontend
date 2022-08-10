2df30cc55f5056dd7b7cc63f3f3e478e603d1921
omerfarukbenli <omerfarukbenli@hotmail.com>
2022-08-10 15:10:27 +0300

    Jwt

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import { AuthInterceptor } from './services/auth.intercaptor';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass:AuthInterceptor,
+      multi:true,
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
